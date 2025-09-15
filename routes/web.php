960ef225ad76aa039d17db98a7477c9ff1bc4af5
elsafalen28 <elsafalentina2023@gmail.com>
2025-09-15 13:48:59 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
-// Route ke method home
 Route::get('/home', [PageController::class, 'home']);
-
-// Route ke method about
 Route::get('/about', [PageController::class, 'about']);
